b2fe7d8470ed0248262dbde3b53a502cad8c595f
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-11 11:55:42 +0700

    Upload next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  output: 'standalone',
+  output: process.env.VERCEL ? undefined : 'standalone',
   transpilePackages: ['motion'],
   webpack: (config, {dev, webpack}) => {
     // HMR is disabled in AI Studio via DISABLE_HMR env var.
