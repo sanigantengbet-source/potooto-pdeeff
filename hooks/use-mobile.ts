b0611613fc880974636fa7a49f4dcb68397573fc
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-14 14:00:03 +0700

    Upload hooks/use-mobile.ts

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -2,21 +2,22 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
-function subscribe(callback: () => void) {
-  const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-  mql.addEventListener("change", callback)
-  return () => mql.removeEventListener("change", callback)
-}
-
-function getSnapshot() {
-  return window.innerWidth < MOBILE_BREAKPOINT
-}
-
-function getServerSnapshot() {
-  return false
-}
-
 export function useIsMobile() {
-  return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
-}
+  const [isMobile, setIsMobile] = React.useState<boolean>(() => {
+    if (typeof window !== "undefined") {
+      return window.innerWidth < MOBILE_BREAKPOINT
+    }
+    return false
+  })
 
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+    mql.addEventListener("change", onChange)
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
+
+  return isMobile
+}
